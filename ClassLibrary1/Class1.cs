61205e6c72fc9ea2673d70eba90f7d0ef52468fd
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-08-27 19:07:10 -0500

    Importing Aggregate.Sdk experiment into git

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private readonly JObject obj;
+
+        public Class1(JObject obj)
+        {
+            this.obj = obj;
+        }
+    }
+}
---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Reactive.Subjects;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        private readonly Subject<JObject> subject;
+
+        public Class1(Subject<JObject> subject)
+        {
+            this.subject = subject;
+        }
+    }
+}
