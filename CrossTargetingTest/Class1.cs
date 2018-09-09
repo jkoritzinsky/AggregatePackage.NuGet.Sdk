aeaec105b1a1834cf1ccb439f509fee310d4e76a
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-09-08 23:53:50 -0500

    Remove old manual testing setup

--- CrossTargetingTest/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CrossTargetingTest
-{
-    public class Class1
-    {
-    }
-}
--- NonEmbeddedLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NonEmbeddedLibrary
-{
-    public class Class1
-    {
-    }
-}
